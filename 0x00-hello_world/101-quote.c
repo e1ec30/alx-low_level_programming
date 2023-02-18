1e9f90da47b6a36d233a7b5b5617d8d64fdc86c6
electro <thelastelectro@gmail.com>
2023-02-18 19:02:17 +0100

    unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, msg, strlen(msg));
+	return (1);
+}
