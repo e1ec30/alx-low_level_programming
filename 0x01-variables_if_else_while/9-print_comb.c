93a66c792974231db8de027ee20ac73dadb8b673
electro <thelastelectro@gmail.com>
2023-02-18 20:09:09 +0100

    number 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		for (y = '0'; y <= '9'; y++)
+		{
+			putchar(x);
+			putchar(y);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
