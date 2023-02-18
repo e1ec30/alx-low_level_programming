e4bf6584b9d2e6caf2dbd899df2c6c6c04a2fe7e
electro <thelastelectro@gmail.com>
2023-02-18 20:11:51 +0100

    number 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,17 +8,13 @@
 int main(void)
 {
 	int x;
-	int y;
 
 	for (x = '0'; x <= '9'; x++)
 	{
-		for (y = '0'; y <= '9'; y++)
-		{
-			putchar(x);
-			putchar(y);
-			putchar(',');
-			putchar(' ');
-		}
+		putchar(x);
+		putchar(',');
+		putchar(' ');
 	}
+	putchar('\n');
 	return (0);
 }
