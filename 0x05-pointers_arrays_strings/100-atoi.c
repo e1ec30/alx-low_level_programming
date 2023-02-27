c5cba15268199f5f46efeff5f59166d4d136ac0c
electro <thelastelectro@gmail.com>
2023-02-27 14:20:17 +0100

    project 5

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _atoi - convert string to number
+ *
+ * @s: string to convert
+ * Return: integer
+ */
+
+int _atoi(char *s)
+{
+	int x;
+
+	x = atoi(s);
+	return (x);
+}
