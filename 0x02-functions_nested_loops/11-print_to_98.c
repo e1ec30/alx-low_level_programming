36af68d93f1ab5dbaadaada52ec7fe32107718d7
electro <thelastelectro@gmail.com>
2023-03-01 19:20:02 +0100

    rest

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -23,11 +23,15 @@ void print_to_98(int n)
 	{
 		if (n > 98)
 		{
-			for (x = n; x > 98; x++)
+			for (x = n; x > 98; x--)
 			{
 				printf("%d, ", x);
 			}
 			printf("%d\n", 98);
+		}
+		else
+		{
+			puts("98");
 		}
 	}
 }
