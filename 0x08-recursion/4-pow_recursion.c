f54f983b69633d13f815a879425c2bf530643b82
electro <thelastelectro@gmail.com>
2023-03-09 03:08:37 +0100

    4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - calculate x ^ y using recursion
+ *
+ * @x: x
+ * @y: y
+ * Return: x ^ y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
