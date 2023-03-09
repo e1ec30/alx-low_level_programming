507674bca75382c2817d5a24c922ba64cb153967
electro <thelastelectro@gmail.com>
2023-03-09 04:05:21 +0100

    6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,8 +9,6 @@
 
 int is_prime_number(int n)
 {
-	if (n <= 1)
-		return (0);
 	return (is_prime(n, 2));
 }
 
@@ -24,9 +22,9 @@ int is_prime_number(int n)
 
 int is_prime(int num, int b)
 {
-	if (b >= num)
+	if (b >= num && b > 1)
 		return (1);
-	else if (num % b == 0)
+	else if (num % b == 0 || b <= 1)
 		return (0);
 	else
 		return (is_prime(num, b++));
