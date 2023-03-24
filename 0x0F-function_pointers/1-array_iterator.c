a083a8e76d54c024590631f0d6066254a7f52067
electro <thelastelectro@gmail.com>
2023-03-24 11:20:38 +0100

    1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * array_iterator - Iterate over array contents
+ *
+ * @array: pointer to array
+ * @size: size of array
+ * @action: action to perform
+ * Return: nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
